f3e3434b7dd1579e33373bc11fb4729079462263
Guy D <guyd@satellogic.com>
2017-05-10 09:54:11 +0300

    changing time lib

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -1,7 +1,7 @@
 import mimetypes
 import os.path
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import mktime
 
 from django.core.exceptions import ImproperlyConfigured
@@ -104,8 +104,8 @@ class AzureStorage(Storage):
             sasToken = None
 
             if expire:
-                today = datetime.datetime.utcnow()
-                todayPlusDelta = today + datetime.timedelta(seconds=expire)
+                today = datetime.utcnow()
+                todayPlusDelta = today + timedelta(seconds=expire)
                 todayPlusDelta = todayPlusDelta.isoformat()
                 sasToken = self.connection.generate_shared_access_signature(self.azure_container, name,
                                                                             SharedAccessPolicy(
