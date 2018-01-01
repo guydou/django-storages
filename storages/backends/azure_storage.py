a4ed2eb8f37648a17c69bbcb765e01b10939ff45
Guy D <guyd@satellogic.com>
2018-01-01 10:44:52 +0200

    proving setting to control the number of connections

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from azure.storage.blob import BlobBlock
 import os.path
 import mimetypes
 from django.core.files.storage import Storage
@@ -8,12 +7,9 @@ from azure.storage import CloudStorageAccount
 from storages.utils import setting
 from tempfile import NamedTemporaryFile
 from django.core.files.base import File
-from django.utils.encoding import force_bytes
 
 from azure.common import AzureMissingResourceHttpError
 from azure.storage.blob import ContentSettings
-import base64
-from django.utils.six.moves import urllib
 
 
 def clean_name(name):
@@ -33,11 +29,6 @@ class AzureStorageFile(File):
         self._storage = storage
         self._is_dirty = False
         self._file = None
-        if 'w' in self._mode:
-            self._storage.connection._put_blob(self._storage.azure_container, self._name, None)
-        self._write_counter = 0
-        self._block_list = list()
-        self._last_commit_pos = 0
 
     def _get_file(self):
         if self._file is None:
@@ -48,8 +39,8 @@ class AzureStorageFile(File):
             if 'r' in self._mode:
                 self._is_dirty = False
                 self._storage.connection.get_blob_to_path(container_name=self._storage.azure_container,
-                                                            blob_name=self._name, file_path=self._file.name,
-                                                            max_connections=10)
+                                                          blob_name=self._name, file_path=self._file.name,
+                                                          max_connections=setting("AZURE_READ_MAX_CONNECTIONS", 2))
 
                 self._file.seek(0)
         return self._file
@@ -65,43 +56,12 @@ class AzureStorageFile(File):
         if 'w' not in self._mode:
             raise AttributeError("File was not opened in write mode.")
         self._is_dirty = True
-        ret = super(AzureStorageFile, self).write(force_bytes(content))
-        if self._needs_flush():
-            self._flush_all_buffers()
+        ret = super(AzureStorageFile, self).write(content)
         return ret
-
-    def _needs_flush(self, current_pos=None):
-        if not(current_pos):
-            current_pos = self.file.tell()
-        buffer_size = current_pos - self._last_commit_pos
-        ret_val = buffer_size >= self._storage.buffer_size
-        return ret_val
-
-    def _flush_buffer(self):
-        self._write_counter += 1
-        block_id = force_bytes(pad_left("{}{}".format(self._name, self._write_counter), 32), 'utf-8')
-        block_id = base64.urlsafe_b64encode(block_id)
-        block_id = urllib.parse.quote_plus(block_id)
-        self.file.seek(self._last_commit_pos)
-        content = self.file.read(self._storage.buffer_size)
-        self._storage.connection.put_block(self._storage.azure_container, self._name,
-                                           content, block_id)
-        self._block_list.append(BlobBlock(block_id))
-        self._last_commit_pos = self.file.tell()
-
-    def _flush_all_buffers(self):
-        """
-        Flushes the write buffer.
-        """
-        pos_before_flush = self.file.tell()
-        while self._needs_flush(pos_before_flush):
-            self._flush_buffer()
-        self.file.seek(pos_before_flush)
 
     def close(self):
         if self._is_dirty:
-            self._flush_buffer()
-            self._storage.connection.put_block_list(self._storage.azure_container, self._name, self._block_list)
+            self._storage.connection.create_blob_from_path(self._storage.azure_container, self._name, self._file.name)
         if self._file is not None:
             self._file.close()
             self._file = None
@@ -113,8 +73,6 @@ class AzureStorage(Storage):
     account_key = setting("AZURE_ACCOUNT_KEY")
     azure_container = setting("AZURE_CONTAINER")
     azure_ssl = setting("AZURE_SSL")
-    max_memory_size = setting('AZURE_BLOB_MAX_MEMORY_SIZE', 0)
-    buffer_size = setting('AZURE_FILE_BUFFER_SIZE', 4194304)
 
     def __init__(self, *args, **kwargs):
         super(AzureStorage, self).__init__(*args, **kwargs)
@@ -162,7 +120,9 @@ class AzureStorage(Storage):
         self.connection.create_blob_from_stream(container_name=self.azure_container,
                                                 blob_name=name,
                                                 stream=content,
-                                                content_settings=content_settings)
+                                                content_settings=content_settings,
+                                                max_connections=setting("AZURE_WRITE_MAX_CONNECTIONS", 2)
+                                                )
         return name
 
     def _expire_at(self, expire):
