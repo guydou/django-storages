f64acc0cbffe216c56919d5cd57c85555074111a
nitely <ecastroborsani@gmail.com>
2018-07-25 17:14:26 -0300

    fixes

--- storages/backends/azure_storage.py
+++ storages/backends/azure_storage.py
@@ -1,78 +1,64 @@
-from azure.storage.blob import BlobBlock
+from __future__ import unicode_literals
+from datetime import datetime, timedelta
+from tempfile import SpooledTemporaryFile
 import os.path
 import mimetypes
-import os.path
-from datetime import datetime, timedelta
+import base64
+import re
 
-from django.core.exceptions import ImproperlyConfigured
+from azure.storage.common import CloudStorageAccount
+from azure.storage.blob import BlobBlock
+from azure.common import AzureMissingResourceHttpError
+from azure.storage.blob import ContentSettings, BlobPermissions
+from storages.utils import setting
 
-try:
-    import azure  # noqa
-except ImportError:
-    raise ImproperlyConfigured(
-        "Could not load Azure bindings. "
-        "See https://github.com/WindowsAzure/azure-sdk-for-python")
-
-try:
-    # azure-storage 0.20.0
-    from azure.storage.blob.blobservice import BlobService
-    from azure.common import AzureMissingResourceHttpError
-except ImportError:
-    from azure.storage import BlobService
-    from azure import WindowsAzureMissingResourceError as AzureMissingResourceHttpError
 from django.core.files.storage import Storage
 from django.utils.deconstruct import deconstructible
-from azure.storage import CloudStorageAccount
-from storages.utils import setting
-from tempfile import SpooledTemporaryFile
 from django.core.files.base import File
 from django.utils.encoding import force_bytes
-
-from azure.common import AzureMissingResourceHttpError
-from azure.storage.blob import ContentSettings
-import base64
 from django.utils.six.moves import urllib
+from django.utils import timezone
+from django.utils.encoding import force_text
 
 
 def clean_name(name):
     return os.path.normpath(name).replace("\\", "/")
 
 
-def pad_left(n, width, pad="0"):
-    return ((pad * width) + str(n))[-width:]
-
-
 @deconstructible
 class AzureStorageFile(File):
 
     def __init__(self, name, mode, storage):
-        self._name = name
+        self.name = name
         self._mode = mode
         self._storage = storage
         self._is_dirty = False
         self._file = None
         if 'w' in self._mode:
-            self._storage.connection._put_blob(self._storage.azure_container, self._name, None)
+            self._storage.connection._put_blob(
+                self._storage.azure_container, self.name, None)
         self._write_counter = 0
         self._block_list = list()
         self._last_commit_pos = 0
 
     def _get_file(self):
-        if self._file is None:
-            self._file = SpooledTemporaryFile(
-                max_size=self._storage.max_memory_size,
-                suffix=".AzureBoto3StorageFile",
-                dir=setting("FILE_UPLOAD_TEMP_DIR", None)
-            )
-            if 'r' in self._mode:
-                self._is_dirty = False
-                # I set max connection to 1 since spooledtempfile is not seekable which is required if we use
-                # max_conection > 1
-                self._storage.connection.get_blob_to_stream(container_name=self._storage.azure_container,
-                                                            blob_name=self._name, stream=self._file,
-                                                            max_connections=1)
+        if self._file is not None:
+            return self._file
 
-                self._file.seek(0)
+        self._file = SpooledTemporaryFile(
+            max_size=self._storage.max_memory_size,
+            suffix=".AzureStorageFile",
+            dir=setting("FILE_UPLOAD_TEMP_DIR", None))
+        if 'r' in self._mode:
+            self._is_dirty = False
+            # I set max connection to 1 since spooledtempfile is not seekable which is required if we use
+            # max_conection > 1
+            self._storage.connection.get_blob_to_stream(
+                container_name=self._storage.azure_container,
+                blob_name=self.name,
+                stream=self._file,
+                max_connections=1)
+            self._file.seek(0)
         return self._file
 
     file = property(_get_file)
@@ -83,6 +69,8 @@ class AzureStorageFile(File):
         return super(AzureStorageFile, self).read(*args, **kwargs)
 
     def write(self, content):
+        if len(content) > 100*1024*1024:
+            raise ValueError("Max chunk size is 100MB")
         if 'w' not in self._mode:
             raise AttributeError("File was not opened in write mode.")
         self._is_dirty = True
@@ -100,13 +88,13 @@ class AzureStorageFile(File):
 
     def _flush_buffer(self):
         self._write_counter += 1
-        block_id = force_bytes(pad_left("{}{}".format(self._name, self._write_counter), 32), 'utf-8')
+        block_id = '{:064d}'.format(self._write_counter).encode('utf-8')
         block_id = base64.urlsafe_b64encode(block_id)
         block_id = urllib.parse.quote_plus(block_id)
         self.file.seek(self._last_commit_pos)
         content = self.file.read(self._storage.buffer_size)
-        self._storage.connection.put_block(self._storage.azure_container, self._name,
-                                           content, block_id)
+        self._storage.connection.put_block(
+            self._storage.azure_container, self.name, content, block_id)
         self._block_list.append(BlobBlock(block_id))
         self._last_commit_pos = self.file.tell()
 
@@ -120,16 +108,47 @@ class AzureStorageFile(File):
         self.file.seek(pos_before_flush)
 
     def close(self):
+        if self._file is None:
+            return
         if self._is_dirty:
             self._flush_buffer()
-            self._storage.connection.put_block_list(self._storage.azure_container, self._name, self._block_list)
-        if self._file is not None:
-            self._file.close()
-            self._file = None
+        # Commit blobs
+        self._storage.connection.put_block_list(
+            self._storage.azure_container,
+            self.name,
+            self._block_list)
+        self._file.close()
+        self._file = None
+
+
+def _content_type(name, content):
+    try:
+        return content.file.content_type
+    except AttributeError:
+        pass
+    try:
+        return content.content_type
+    except AttributeError:
+        pass
+    return mimetypes.guess_type(name)[0]
+
+
+def _get_valid_filename(s):
+    # A blob name:
+    #   * must not end with dot or slash
+    #   * can contain any character
+    #   * must escape URL reserved characters
+    # We allow a subset of this to avoid
+    # illegal file names.
+    # Note `/../` does nothing malicious
+    # since azure has virtual paths
+    s = force_text(s).strip(' ./')
+    return re.sub(r'(?u)[^-_\w./ ]', '', s)
 
 
 @deconstructible
 class AzureStorage(Storage):
+
     account_name = setting("AZURE_ACCOUNT_NAME")
     account_key = setting("AZURE_ACCOUNT_KEY")
     azure_container = setting("AZURE_CONTAINER")
@@ -137,8 +156,7 @@ class AzureStorage(Storage):
     max_memory_size = setting('AZURE_BLOB_MAX_MEMORY_SIZE', 0)
     buffer_size = setting('AZURE_FILE_BUFFER_SIZE', 4194304)
 
-    def __init__(self, *args, **kwargs):
-        super(AzureStorage, self).__init__(*args, **kwargs)
+    def __init__(self):
         self._connection = None
 
     @property
@@ -157,8 +175,8 @@ class AzureStorage(Storage):
     def _open(self, name, mode="rb"):
         return AzureStorageFile(name, mode, self)
 
-    def exists(self, file_name):
-        return self.connection.exists(self.azure_container, file_name)
+    def exists(self, name):
+        return self.connection.exists(self.azure_container, name)
 
     def delete(self, name):
         try:
@@ -171,50 +189,107 @@ class AzureStorage(Storage):
             self.azure_container, name).properties
         return properties.content_length
 
-    def _save(self, name, content):
-        if hasattr(content.file, 'content_type'):
-            content_type = content.file.content_type
-        else:
-            content_type = mimetypes.guess_type(name)[0]
+    def get_valid_name(self, name):
+        """
+        Returns a filename, based on the provided filename, that's suitable for
+        use in the target storage system.
+        """
+        # XXX allow up to 256 `/` slashes
+        name = _get_valid_filename(name)
+        if len(name) > 1024:
+            raise ValueError("Blob name max len is 1024")
+        if not len(name):
+            raise ValueError(
+                "A file name of one or more"
+                "printable characters is required")
+        return name
 
-        # if hasattr(content, 'chunks'):
-            # content = BytesIO(b''.join(chunk for chunk in content.chunks()))
+    def get_available_name(self, name, max_length=None):
+        """
+        Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+        """
+        name = self.get_valid_name(name)
+        return super(AzureStorage, self).get_available_name(name, max_length)
+
+    def save(self, name, content, content_type=None):
+        name = self.get_valid_name(name)
+
+        if content_type is None:
+            content_type = _content_type(name, content)
+
         content_settings = ContentSettings(content_type=content_type)
-        self.connection.create_blob_from_stream(container_name=self.azure_container,
-                                                blob_name=name,
-                                                stream=content,
-                                                content_settings=content_settings)
+        self.connection.create_blob_from_stream(
+            container_name=self.azure_container,
+            blob_name=name,
+            stream=content,
+            content_settings=content_settings)
         return name
 
     def _expire_at(self, expire):
-            now = datetime.utcnow()
-            now_plus_delta = now + timedelta(seconds=expire)
-            now_plus_delta = now_plus_delta.replace(microsecond=0).isoformat() + 'Z'
-            return now, now_plus_delta
+        now = datetime.utcnow()
+        now_plus_delta = now + timedelta(seconds=expire)
+        now_plus_delta = now_plus_delta.replace(microsecond=0).isoformat() + 'Z'
+        return now, now_plus_delta
 
-    def url(self, name, expire=None, mode='r'):
-        if hasattr(self.connection, 'make_blob_url'):
-            sas_token = None
-            make_blob_url_kwargs = {}
-            if expire:
-                now, now_plus_delta = self._expire_at(expire)
-                sas_token = self.connection.generate_blob_shared_access_signature(self.azure_container,
-                                                                                  name, 'r',
-                                                                                  expiry=now_plus_delta)
-                make_blob_url_kwargs['sas_token'] = sas_token
+    def url(self, name, expire=None):
+        make_blob_url_kwargs = {}
+        if expire:
+            now, now_plus_delta = self._expire_at(expire)
+            sas_token = self.connection.generate_blob_shared_access_signature(
+                self.azure_container, name, BlobPermissions.READ, expiry=now_plus_delta)
+            make_blob_url_kwargs['sas_token'] = sas_token
 
-            if self.azure_protocol:
-                make_blob_url_kwargs['protocol'] = self.azure_protocol
-            return self.connection.make_blob_url(
-                container_name=self.azure_container,
-                blob_name=name,
-                **make_blob_url_kwargs
-            )
-        else:
-            return "{}{}/{}".format(setting('MEDIA_URL'), self.azure_container, name)
+        if self.azure_protocol:
+            make_blob_url_kwargs['protocol'] = self.azure_protocol
+        return self.connection.make_blob_url(
+            container_name=self.azure_container,
+            blob_name=name,
+            **make_blob_url_kwargs)
 
-    def modified_time(self, name):
+    def get_modified_time(self, name):
+        """
+        Returns an (aware) datetime object containing the last modified time if
+        USE_TZ is True, otherwise returns a naive datetime in the local timezone.
+        """
         properties = self.connection.get_blob_properties(
             self.azure_container, name).properties
-        modified = properties.last_modified
-        return modified
+        if setting('USE_TZ'):
+            # `last_modified` is in UTC time_zone, we
+            # must convert it to settings time_zone
+            tz = timezone.get_current_timezone()
+            return properties.last_modified.astimezone(tz)
+        else:
+            return timezone.make_naive(properties.last_modified)
+
+    def modified_time(self, name):
+        """Returns a naive datetime object containing the last modified time."""
+        mtime = self.get_modified_time(name)
+        if timezone.is_naive(mtime):
+            return mtime
+        return timezone.make_naive(mtime)
+
+    def list_all(self, path=''):
+        """Return all files for a given path"""
+        return [
+            blob.name
+            for blob in self.connection.list_blobs(
+                self.azure_container, prefix=path)]
+
+    def listdir(self, path=''):
+        """
+        Return directories and files for a given path.
+        Leave the path empty to list the root.
+        Order of dirs and files is undefined.
+        """
+        if path and not path.endswith('/'):
+            path += '/'
+        files = []
+        dirs = set()
+        for name in self.list_all(path):
+            n = name[len(path):]
+            if '/' in n:
+                dirs.add(n.split('/', 1)[0])
+            else:
+                files.append(n)
+        return list(dirs), files
